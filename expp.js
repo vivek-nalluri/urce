f8c31bab6b16814f99cab5aab4513aa1fea18f2f
vivek-nalluri <137318292+vivek-nalluri@users.noreply.github.com>
2024-11-22 11:22:53 +0530

    Add files via upload

---
+++ expp.js
@@ -0,0 +1,18 @@
+for (i = 1; i = 10; i++) {
+    let a = document.getElementById("body");
+  setTimeout(()=> {
+    a.style.backgroundColor = "red";
+  }, 2000);
+
+
+
+  
+//   setTimeout(function () {
+    
+//     a.style.backgroundColor = "black";
+//   }, 2000);
+//   setTimeout(function () {
+   
+//     a.style.backgroundColor = "blue";
+//   }, 2000);
+}
